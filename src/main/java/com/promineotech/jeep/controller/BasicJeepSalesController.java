01f0c70e2d10bd5a872a779b8d8bec02977b1e1f
Ken Bernstein <112273610+kenb2489@users.noreply.github.com>
2022-12-15 21:00:03 -0700

    Add files via upload

---
+++ src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
@@ -0,0 +1,15 @@
+package com.promineotech.jeep.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+@RestController
+public class BasicJeepSalesController implements JeepSalesController {
+
+  public List<Jeep> fetchJeeps(JeepModel model, String trim) {
+    return null;
+  }
+
+}
---
+++ src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,6 @@
+package com.promineotech.jeep.entity;
+
+public enum JeepModel {
+  //SELECT DISTINCT model_id FROM models;
+  WRANGLER, CRAND_CHEROKEE, CHEROKEE, COMPESS, RENEGADE, GLADIATOR, WRANGLER_4XE
+}
